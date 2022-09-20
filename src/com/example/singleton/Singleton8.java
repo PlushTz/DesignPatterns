44136dc884f6a82c45d2a59950ca2befe05e9d62
李锦涛 <tao351992257@163.com>
2022-09-20 23:53:01 +0800

    Commit 单例模式的8种写法

---
+++ src/com/example/singleton/Singleton8.java
@@ -0,0 +1,16 @@
+package com.example.singleton;
+
+/**
+ * Desc:
+ * 8.枚举【推荐使用】
+ * 借助JDK1.5中添加的枚举来实现单例模式。不仅能避免多线程同步问题，而且还能防止反序列化重新创建新的对象。
+ * @author Coco
+ * Created on 2022/9/20 23:51
+ * Email:
+ */
+public enum Singleton8 {
+    INSTANCE;
+    public void method(){
+
+    }
+}
